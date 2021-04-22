966471aae83a8d499a9a7d735ffa98d88b77882b
Jiří Nohavec <jnohavec@samepage.io>
2021-04-23 01:08:20 +0200

    Update cpt.js

--- www/cpt.js
+++ www/cpt.js
@@ -2,7 +2,6 @@ const exec = require('cordova/exec');
 
 //-------------------------------------------------------------------------------------------------
 
-/* returns OK */
 exports.f = function(callback) {
 	if (callback) {
 		callback('ok');
